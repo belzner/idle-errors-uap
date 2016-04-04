226105301b778ec29d49ee7de4d20f8419316d7e
Megan Belzner <belzner@mit.edu>
2016-04-03 21:45:17 -0400

    Check for reserved word as variable name error

--- SyntaxErrorHelper.py
+++ SyntaxErrorHelper.py
@@ -1,10 +1,12 @@
 from tkinter import END
 import io
 import tokenize
+import keyword
 
 from HyperParser import HyperParser
 
 ERR_PAREN = "You may have forgotten a closing bracket to match the opening bracket on line "
+ERR_RESERVED = "You may have tried to use a reserved word as a variable name on line "
 
 def writeSyntaxError(shell, value, win):
     text = win.text
@@ -25,6 +27,21 @@ def writeSyntaxError(shell, value, win):
                            .get_surrounding_brackets(mustclose=True))
         if matchedBrackets is None: # No closing bracket
             extramsg = ERR_PAREN + hasBrackets[0].split('.')[0]
+
+    # Check for using reserved word as variable name
+    try:
+        tokens = list(tokenize.tokenize(io.BytesIO(bytes(errorline, 'utf-8')).readline))
+        equalsPos = -1
+        for i in range(len(tokens)):
+            if tokens[i].exact_type is tokenize.EQUAL:
+                equalsPos = i
+                break
+        if equalsPos > 0:
+            for i in range(equalsPos):
+                if tokens[i].type is tokenize.NAME and keyword.iskeyword(tokens[i].string):
+                    extramsg = ERR_RESERVED + str(lineno)
+    except Exception as e:
+        print('error', e)
 
     shell.write("\nSyntaxError: "+msg+"\n"+extramsg+"\n")
     shell.tkconsole.showprompt()
