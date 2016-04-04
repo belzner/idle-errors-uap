5e60628b778fca68b00b8d98f99e79f720748210
Megan Belzner <belzner@mit.edu>
2016-04-03 22:47:02 -0400

    Check for missing colon at end of statement error

--- SyntaxErrorHelper.py
+++ SyntaxErrorHelper.py
@@ -7,6 +7,7 @@ from HyperParser import HyperParser
 
 ERR_PAREN = "You may have forgotten a closing bracket to match the opening bracket on line "
 ERR_RESERVED = "You may have tried to use a reserved word as a variable name on line "
+ERR_COLON = "You may have forgotten a colon at the end of line "
 
 def writeSyntaxError(shell, value, win):
     text = win.text
@@ -28,9 +29,27 @@ def writeSyntaxError(shell, value, win):
         if matchedBrackets is None: # No closing bracket
             extramsg = ERR_PAREN + hasBrackets[0].split('.')[0]
 
-    # Check for using reserved word as variable name
     try:
         tokens = list(tokenize.tokenize(io.BytesIO(bytes(errorline, 'utf-8')).readline))
+
+        # Order of tests matters because of course it does
+
+        # Check for missing colon at end of statement
+        statementName = ''
+        for i in range(len(tokens)):
+            if tokens[i].type is tokenize.NAME:
+                statementName = tokens[i].string
+                break
+        if statementName in ['if', 'elif', 'else', 'for', 'while', 'class', 'def']:
+            hasColon = False
+            for i in range(len(tokens))[::-1]:
+                if tokens[i].exact_type is tokenize.COLON:
+                    hasColon = True
+                    break
+            if not hasColon:
+                extramsg = ERR_COLON + str(lineno)
+
+        # Check for using reserved word as variable name
         equalsPos = -1
         for i in range(len(tokens)):
             if tokens[i].exact_type is tokenize.EQUAL:
