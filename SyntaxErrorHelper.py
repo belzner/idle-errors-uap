bc7089f00c55bf35b0dee62156fcab7855cca9d3
Megan Belzner <belzner@mit.edu>
2016-04-10 15:47:55 -0400

    Add and strengthen syntax checks

--- SyntaxErrorHelper.py
+++ SyntaxErrorHelper.py
@@ -5,9 +5,10 @@ import keyword
 
 from HyperParser import HyperParser
 
-ERR_PAREN = "You may have forgotten a closing bracket to match the opening bracket on line "
-ERR_RESERVED = "You may have tried to use a reserved word as a variable name on line "
-ERR_COLON = "You may have forgotten a colon at the end of line "
+ERR_PAREN = "You may have forgotten a closing bracket to match the opening bracket on line %d"
+ERR_RESERVED = "You may have tried to use the reserved word '%s' as a variable name on line %d"
+ERR_COLON = "You may have forgotten a colon at the end of line %d"
+ERR_ASSIGN = "You may have used '=' when you meant to use a comparison operator (such as '==') on line %d"
 
 def writeSyntaxError(shell, value, win):
     text = win.text
@@ -27,7 +28,7 @@ def writeSyntaxError(shell, value, win):
         matchedBrackets = (HyperParser(win, linepos)
                            .get_surrounding_brackets(mustclose=True))
         if matchedBrackets is None: # No closing bracket
-            extramsg = ERR_PAREN + hasBrackets[0].split('.')[0]
+            extramsg = ERR_PAREN % int(hasBrackets[0].split('.')[0])
 
     try:
         tokens = list(tokenize.tokenize(io.BytesIO(bytes(errorline, 'utf-8')).readline))
@@ -47,7 +48,14 @@ def writeSyntaxError(shell, value, win):
                     hasColon = True
                     break
             if not hasColon:
-                extramsg = ERR_COLON + str(lineno)
+                extramsg = ERR_COLON % lineno
+
+        # Check for using = (assignment) in if/elif/while
+        if statementName in ['if', 'elif', 'while']:
+            for i in range(len(tokens)):
+                if tokens[i].exact_type is tokenize.EQUAL:
+                    extramsg = ERR_ASSIGN % lineno
+                    break
 
         # Check for using reserved word as variable name
         equalsPos = -1
@@ -55,10 +63,15 @@ def writeSyntaxError(shell, value, win):
             if tokens[i].exact_type is tokenize.EQUAL:
                 equalsPos = i
                 break
-        if equalsPos > 0:
-            for i in range(equalsPos):
-                if tokens[i].type is tokenize.NAME and keyword.iskeyword(tokens[i].string):
-                    extramsg = ERR_RESERVED + str(lineno)
+        if equalsPos > 0 and tokens[equalsPos-1].type is tokenize.NAME:
+            for i in range(equalsPos)[::-1]:
+                if tokens[i].type is tokenize.NAME:
+                    if keyword.iskeyword(tokens[i].string):
+                        extramsg = ERR_RESERVED % (tokens[i].string, lineno)
+                        break
+                    else:
+                        if i > 0 and tokens[i-1].exact_type is not tokenize.COMMA:
+                            break
     except Exception as e:
         print('error', e)
 
